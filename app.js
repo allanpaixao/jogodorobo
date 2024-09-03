802d66fe5cd3b1152a87adeac52ee2d8e0828393
Allan Paixão <111083316+allanpaixao@users.noreply.github.com>
2024-09-03 13:05:37 -0300

    Add files via upload
    Jogo de pergunta e resposta bem simples.

---
+++ app.js
@@ -0,0 +1,13 @@
+alert("Boas vindas ao jogo do número secreto");
+let numeroSecreto = 5;
+let chute = prompt("Escolha um número entre 1 é 10");
+
+if(chute == numeroSecreto){
+    console.log(`Isso ai você descobriu o número secreto ${numeroSecreto} `);
+
+}else{
+    alert("você errou");
+}
+
+
+
